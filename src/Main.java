c387911d33a362f8aa5f92183148f816c91b36f9
PigeonPC <olgakatinka@gmail.com>
2025-08-29 16:25:56 +0200

    Rettelse

--- src/Main.java
+++ src/Main.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 public class Main {
     /*
-   1. Create a Java project called TripPlanner in IntelliJ.
-   2. Create a class called Trip with at least 4 fields, ex. country, duration etc.
-   3. Create two subclasses to the Trip class called SkiTrip and BeachTrip.
-      Each subclass should have some encapsulated fields of their own.
-   4. Create a menu with the following options:
+    1. Create a Java project called TripPlanner in IntelliJ.
+    2. Create a class called Trip with at least 4 fields, ex. country, duration etc.
+    3. Create two subclasses to the Trip class called SkiTrip and BeachTrip.
+       Each subclass should have some encapsulated fields of their own.
+    4. Create a menu with the following options:
        - Create trip
        - List trip
        and make the methods for this functionality, (put the created trips in an ArrayList).
        Make it nice and working.
-         5.	Create a local Git Repository,
-         6. Add version control to your code/project, and commit your code to your local repository.
-         7.	Share the project on GitHub
-         8.	Add a new method to your code that gives the user the option to find and alter a made trip.
-         9. Push your changes to GitHub.
-   */
+    5. Create a local Git Repository,
+    6. Add version control to your code/project, and commit your code to your local repository.
+    7. Share the project on GitHub
+    8. Add a new method to your code that gives the user the option to find and alter a made trip.
+    9. Push your changes to GitHub.
+    */
 
     private static ArrayList<Trip> trips = new ArrayList<>();
 
